2892b056f98c72534ee1f60e4b63d11e1c86585e
yash raj <yashraj0207@outlook.com>
2024-09-08 01:21:59 +0530

    update link name

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -14,7 +14,7 @@ export default function Index() {
   return (
     <div className="font-sans p-4">
       <h1 className="text-3xl">Welcome to Remix</h1>
-      <Link to={`/users/${userId}`}>users {userId}</Link>
+      <Link to={`/users/${userId}`}>Random user: {userId}</Link>
     </div>
   );
 }
